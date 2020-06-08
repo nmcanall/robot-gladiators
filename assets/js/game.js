df9d2a6685ddae40ff5dfce2208f1c4fa9c1993e
nmcanall <nmcanall@citadel.edu>
2020-06-07 18:25:11 -0700

    fixed blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,9 +151,18 @@ var randomNumber = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Helper function to set the name 
+var getPlayerName = function() {
+    var name = "";
+    while(name === "" || name === null) {
+        name = window.prompt("What is your robot's name?");
+    }
+    return name;
+}
+
 // Player object
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
